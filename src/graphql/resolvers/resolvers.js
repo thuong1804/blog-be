b3ca4a3f26cde65f3fa7bee2bd00efbf8c39a904
thuonglh <thuongtvt30@gmail.com>
2025-10-15 16:19:50 +0700

    Feat: Implements create and delete post functionality
    Adds functionality to create and delete posts, including markdown content conversion.
    Also, include get tags functionality.

--- src/graphql/resolvers/resolvers.js
+++ src/graphql/resolvers/resolvers.js
@@ -5,13 +5,15 @@ import { sendMailResolvers } from './sendMail.resolvers.js';
 import { authResolvers } from './auth.resolvers.js';
 import { cloudinaryResolvers } from './cloudinary.resolvers.js';
 import { OTPResolvers } from './otp.resolvers.js';
+import {tagResolvers} from "./tag.resolvers.js"
 
 const resolvers = {
   Query: {
     ...postResolvers.Query,
     ...categoryResolvers.Query,
     ...userResolvers.Query,
-    ...cloudinaryResolvers.Query
+    ...cloudinaryResolvers.Query,
+    ...tagResolvers.Query,
   },
   Mutation: {
     ...sendMailResolvers.Mutation,
@@ -19,6 +21,7 @@ const resolvers = {
     ...cloudinaryResolvers.Mutation,
     ...userResolvers.Mutation,
     ...OTPResolvers.Mutation,
+    ...postResolvers.Mutation
   },
 };
 
