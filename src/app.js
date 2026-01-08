43f823b9607c9656154ef74b42fb75ae76ab81bb
thuonglh <thuongtvt30@gmail.com>
2026-01-08 15:52:59 +0700

    chore: Clean up app.js for better readability and organization

--- src/app.js
+++ src/app.js
@@ -18,6 +18,7 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
 const app = express();
+await apolloServer.start();
 
 app.use(express.static("public"));
 
@@ -29,14 +30,7 @@ app.use(
 );
 app.use(express.json());
 
-app.use((req, res, next) => {
-    if (!req.body && process.env.NODE_ENV !== "production") {
-        req.body = {};
-    }
-    next();
-});
-
-await apolloServer.start();
+app.use(express.urlencoded({ extended: true }));
 
 app.use(
     "/graphql",
